f29b5df959a681539f641bb37a21fe827d78477f
mike dupont <mike.dupont@introspector.local>
2024-12-13 12:14:54 -0500

    bugfix egress

--- environments/swarms-aws-agent-api/dev/us-east-1/components/machine_image/main.tf
+++ environments/swarms-aws-agent-api/dev/us-east-1/components/machine_image/main.tf
@@ -51,46 +51,46 @@ locals {
   }
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = ["099720109477"] # Ubuntu's account ID
-  filter {
-    name   = "name"
-    values = [
-      #"ubuntu/images/hvm-ssd/ubuntu*24*amd64-server*"
-      "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*",
-    ]
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   owners = ["099720109477"] # Ubuntu's account ID
+#   filter {
+#     name   = "name"
+#     values = [
+#       #"ubuntu/images/hvm-ssd/ubuntu*24*amd64-server*"
+#       "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*",
+#     ]
 
-# from https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#AMICatalog:https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#AMICatalog:    
-# ubuntu (2 filtered, 8 unfiltered)
-# Free tier eligible
-# Ubuntu Server 24.04 LTS (HVM), SSD Volume Type
-# ami-0e2c8caa4b6378d8c (64-bit (x86)) / ami-0932ffb346ea84d48 (64-bit (Arm))
-# Platform: ubuntu
-# Root device type: ebs
-# Virtualization: hvm
-# ENA enabled: Yes
-# Select
-# 64-bit (x86)
-# 64-bit (Arm)
-# Ubuntu
+# # from https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#AMICatalog:https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#AMICatalog:    
+# # ubuntu (2 filtered, 8 unfiltered)
+# # Free tier eligible
+# # Ubuntu Server 24.04 LTS (HVM), SSD Volume Type
+# # ami-0e2c8caa4b6378d8c (64-bit (x86)) / ami-0932ffb346ea84d48 (64-bit (Arm))
+# # Platform: ubuntu
+# # Root device type: ebs
+# # Virtualization: hvm
+# # ENA enabled: Yes
+# # Select
+# # 64-bit (x86)
+# # 64-bit (Arm)
+# # Ubuntu
 
 
-# ami-005fc0f236362e99f (64-bit (x86)) / ami-07ee04759daf109de (64-bit (Arm))
-# Ubuntu Server 22.04 LTS (HVM),EBS General Purpose (SSD) Volume Type. 
-# Platform: ubuntu
-# Root device type: ebs
-# Virtualization: hvm
-# ENA enabled: Yes
-# Select
-# 64-bit (x86)
-#   64-bit (Arm)
+# # ami-005fc0f236362e99f (64-bit (x86)) / ami-07ee04759daf109de (64-bit (Arm))
+# # Ubuntu Server 22.04 LTS (HVM),EBS General Purpose (SSD) Volume Type. 
+# # Platform: ubuntu
+# # Root device type: ebs
+# # Virtualization: hvm
+# # ENA enabled: Yes
+# # Select
+# # 64-bit (x86)
+# #   64-bit (Arm)
   
-    #"ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-server-20240823",
-    #"ubuntu-minimal/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-minimal-20240824",
-    #"ubuntu-minimal/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-minimal-20240813",
-  }
-}
+#     #"ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-server-20240823",
+#     #"ubuntu-minimal/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-minimal-20240824",
+#     #"ubuntu-minimal/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-minimal-20240813",
+#   }
+# }
 
 module "security_group_instance" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -114,15 +114,15 @@ module "security_group_instance" {
     }
   ]
 
-  #egress_rules = ["https-443-tcp"]
-  egress_with_cidr_blocks = [
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = "0.0.0.0/0"
-    }
-  ]
+  egress_rules = ["all-all"]
+  # egress_with_cidr_blocks = [
+  #   {
+  #     from_port   = 0
+  #     to_port     = 0
+  #     protocol    = "tcp"
+  #     cidr_blocks = "0.0.0.0/0"
+  #   }
+  # ]
 
   tags = local.tags
 }
