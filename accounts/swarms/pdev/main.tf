c930829768c0f941bc9fb0f214c17e62378ecc76
mike dupont <mike.dupont@introspector.local>
2024-12-22 15:28:52 -0500

    pdev

---
+++ accounts/swarms/pdev/main.tf
@@ -0,0 +1,20 @@
+
+resource "aws_iam_role" "pdev" {
+  name        = "pdev"
+  description = "pdev role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::354918380242:root"
+        }
+        Action = "sts:AssumeRole"
+	"Condition": { "Bool": { "aws:MultiFactorAuthPresent": "true" } }
+      }
+      
+    ]
+  })
+}
